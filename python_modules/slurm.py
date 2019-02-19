0a49926c44a25535eac41683668931104b80efc3
Aaron S. Jackson <aaron@aaronsplace.co.uk>
2019-02-19 22:32:30 +0000

    Added slurm_gpu_free and slurm_gpu_alloc

--- python_modules/slurm.py
+++ python_modules/slurm.py
@@ -1,27 +1,40 @@
 import subprocess
 import time
+import re
 from collections import Counter
 
-cached = None
+cached = {}
 cached_time = 0
 
 def slurm_handler(name):
-    name = name.replace('slurm_jobs_', '')
-
     global cached, cached_time
+    p = name.split('_')
     now = time.time()
-    if cached == None or now-cached_time > 20:
-        w = subprocess.check_output(["squeue", "-h", "-Ostate"]).replace(' ', '').splitlines()
-        cached = dict(Counter(w))
+
+    if now-cached_time > 20:
         cached_time = now
-    if name in cached.keys():
-        return cached[name]
+        w = subprocess.check_output(["squeue", "-h", "-Ostate"])
+        w = w.replace(' ', '').splitlines()
+        cached['jobs'] = dict(Counter(w))
+
+        w = subprocess.check_output(["scontrol", "show", "node", "-o"]).splitlines()
+        cached['gpu'] = {'alloc': 0, 'free': 0}
+        for l in w:
+            alloc = re.search('.*AllocTRES=([\w=,]*)gres/gpu=([0-9]?).*', l)
+            alloc = int(alloc.group(2)) if alloc != None else 0
+            cfg = re.search('.*CfgTRES=([\w=,]*)gres/gpu=([0-9]?).*', l)
+            cfg = int(cfg.group(2)) if cfg != None else 0
+            cached['gpu']['free'] += (int(cfg) - int(alloc))
+            cached['gpu']['alloc'] += int(alloc)
+
+    if p[1] in cached.keys() and p[2] in cached[p[1]].keys():
+        return cached[p[1]][p[2]]
     else:
         return 0
 
 def metric_init(params):
     global descriptors
-    
+
     d1 = {'name': 'slurm_jobs_RUNNING',
           'call_back': slurm_handler,
           'time_max': 20,
@@ -42,7 +55,27 @@ def metric_init(params):
           'description': 'number of pending slurm jobs',
           'groups': 'health'}
 
-    descriptors = [d1, d2]
+    d3 = {'name': 'slurm_gpu_free',
+          'call_back': slurm_handler,
+          'time_max': 20,
+          'value_type': 'uint',
+          'units': 'jobs',
+          'slope': 'both',
+          'format': '%u',
+          'description': 'number of free gpus',
+          'groups': 'health'}
+
+    d4 = {'name': 'slurm_gpu_alloc',
+          'call_back': slurm_handler,
+          'time_max': 20,
+          'value_type': 'uint',
+          'units': 'jobs',
+          'slope': 'both',
+          'format': '%u',
+          'description': 'number of alloc gpus',
+          'groups': 'health'}
+
+    descriptors = [d1, d2, d3, d4]
 
     return descriptors
 
